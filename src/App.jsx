8922587fcbc589db9ab5ef381d98c0cbc67caacd
Filip <filip.a.dabrowski@gmail.com>
2024-07-31 17:43:46 +0200

    [Update] added comments + final check

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import Counter from './Couter';
+import Counter from './Counter';
 import './App.css';
 
 function App() {
@@ -7,14 +7,15 @@ function App() {
   // état initial : la valeur de compteur = 0
   const [count, setCount] = useState(0);
 
-  // les fonctions changeant l'état (+ et -)
+  // les fonctions changeant l'état (deux fonctions: + et -)
   const onClickAdd = () => setCount(count + 1);
   const onClickSubtract = () => setCount(count - 1);
 
-  // affichage
+  // affichage / interface
+  // ajourer 2 props à Counter en utilisant des boutons
   return (
     <div>
-      <h1>Compteur: {count}</h1>
+      <h1>Compteur: {count}</h1> 
       <Counter onClickAdd={onClickAdd} onClickSubtract={onClickSubtract} />
     </div>
   );
--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 // composant a 2 props : onClickAdd et onClickSubtract
+// les props sont ajoutés dans App en utilisant des boutons
 const Counter = ({ onClickAdd, onClickSubtract }) => {
   return (
     <div>
